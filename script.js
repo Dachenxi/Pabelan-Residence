ab8e9faaeac4605e0d3f2a7658931eddffd1e611
Putra <70789166+Dachenxi@users.noreply.github.com>
2025-10-08 10:19:46 +0700

    Nambahin Icon Dan ganti smooth scrolling dari css menggunakan java script.js

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function initNavigation() {
     const navLink = document.querySelectorAll('.menu .nav-link')
-
+    const header = document.querySelector('.header')
+    
     navLink.forEach(link => {
-        link.addEventListener('click', () => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault()
+            const targetId = link.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            
+            if (targetElement) {
+                const headerHeight = header ? header.offsetHeight : 0;
+                const targetPosition = targetElement.offsetTop - headerHeight;
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+            
             navLink.forEach(l => l.classList.remove('active'));
             link.classList.add('active');
         });
