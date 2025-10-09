fed8a9cfeff56ec532aa1d023b340bdabb4cee6e
Putra <70789166+Dachenxi@users.noreply.github.com>
2025-10-09 17:30:33 +0700

    Mengubah navbar menjadi responsive

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+    initHamburgerMenu();
     initNavigation();
 })
 
 function initNavigation() {
     const navLink = document.querySelectorAll('.menu .nav-link')
     const header = document.querySelector('.header')
-    
+    const navbarMenu = document.getElementById('navbar-menu')
+
     navLink.forEach(link => {
-        link.addEventListener('click', (event) => {
+        link.addEventListener('click', async (event) => {
             event.preventDefault()
             const targetId = link.getAttribute('href');
             const targetElement = document.querySelector(targetId);
-            
+
             if (targetElement) {
                 const headerHeight = header ? header.offsetHeight : 0;
                 const targetPosition = targetElement.offsetTop - headerHeight;
@@ -20,9 +22,23 @@ function initNavigation() {
                     behavior: 'smooth'
                 });
             }
-            
+
             navLink.forEach(l => l.classList.remove('active'));
             link.classList.add('active');
+
+            setTimeout(() => {
+                navbarMenu.classList.remove('active');
+            }, 300);
         });
     })
+}
+
+
+function initHamburgerMenu() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navbarMenu = document.getElementById('navbar-menu');
+
+    menuToggle.addEventListener('click', () => {
+        navbarMenu.classList.toggle('active');
+    });
 }
